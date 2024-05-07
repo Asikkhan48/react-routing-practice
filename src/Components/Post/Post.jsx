9c35cd84d7429970ce680c9218725129a78d38d1
Asikkhan48 <khanasik48@gmail.com>
2024-05-07 13:17:45 +0600

    finish

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,16 +1,25 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 
 const Post = ({post}) => {
 
     const {id, title, body} = post;
+
+    // Navigate Handlers
+    const navigate = useNavigate();
+
+    const handleShowDetails = () =>{
+        navigate(`/post/${id}`)
+    }
+
     return (
         <div className="border-2 border-pink-500 mt-5 rounded-lg p-4">
             <h1>Id: {id}</h1>
             <h3>Title: {title}</h3>
             <p>Body: {body}</p>
             <NavLink to={`/post/${id}`}><button className="btn btn-link">Show Posts</button></NavLink>
-            
+            <br />
+            <button onClick={handleShowDetails}>handler show details</button>
         </div>
     );
 };
--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,15 +1,25 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const PostDetails = () => {
     const post = useLoaderData();
+    const navigate = useNavigate();
     const {id,  title, body} =post;
+
+    // Go Back
     
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
+
     return (
         <div className="border-2 border-pink-500 mt-5 rounded-lg p-4">
             <h1>Id: {id}</h1>
             <h3>Title: {title}</h3>
             <p>Body: {body}</p>
+            <br />
+            <button className="btn btn-success" onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
